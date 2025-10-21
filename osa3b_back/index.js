324af9e333a85c9b065fba41685723f3ac9685a9
Kotvau <Peppina.Henriksson@student.lab.fi>
2025-10-21 13:44:40 +0300

    dist yms

--- osa3b_back/index.js
+++ osa3b_back/index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express();
 const cors = require("cors");
-
+app.use(express.static("dist"));
 const morgan = require("morgan");
 app.use(cors());
 app.use(express.json());
--- osa3b_front/src/services/persons.js
+++ osa3b_front/src/services/persons.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-const baseUrl = "http://localhost:3001/api/persons"; //hakee backistä
+const baseUrl = "/api/persons"; //hakee backistä
 
 const getAll = () => {
   const request = axios.get(baseUrl);
